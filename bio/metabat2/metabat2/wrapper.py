92598cd33cc7687b251313025e527775124fc86a
Alex Huebner <alexhbnr@gmail.com>
2022-04-13 15:00:55 +0200

    Escape low-quality assembly that fail to bin with metaBAT2

--- bio/metabat2/metabat2/wrapper.py
+++ bio/metabat2/metabat2/wrapper.py
@@ -2,20 +2,35 @@ __author__ = "Alexander Hübner"
 __copyright__ = "Copyright 2022, Alexander Hübner"
 __license__ = "MIT"
 
-
 from snakemake.shell import shell
 
 minlength = snakemake.params.get("minlength", "")
 outprefix = snakemake.params.get("outprefix", "")
 
-shell(
-    "(metabat2 "
-    " -i {snakemake.input[0]}"
-    " -o {outprefix}"
-    " -a {snakemake.input[1]}"
-    " --minContig {minlength}"
-    " --minClsSize 100000"
-    " -t {snakemake.threads}"
-    " --unbinned"
-    " --verbose)"
-)
+with open(snakemake.input[1], "rt") as depthfile:
+    for i, line in enumerate(depthfile):
+        if i > 1:
+            break
+        else:
+            pass
+
+if i > 1:
+
+    shell(
+        "(metabat2 "
+        " -i {snakemake.input[0]}"
+        " -o {outprefix}"
+        " -a {snakemake.input[1]}"
+        " --minContig {minlength}"
+        " --minClsSize 100000"
+        " -t {snakemake.threads}"
+        " --unbinned"
+        " --verbose)"
+    )
+
+else:
+
+    shell(
+        "(gunzip -c {snakemake.input[0]} "
+        "  > {snakemake.output[0]})"
+    )
