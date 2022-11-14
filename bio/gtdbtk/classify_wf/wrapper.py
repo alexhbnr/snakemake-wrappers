834dc1f24c58b138f041cbe0452748f76183c2a4
Alex Huebner <alexhbnr@gmail.com>
2022-11-14 16:27:18 +0100

    Escape case that no bacterial MAG was found

---
+++ bio/gtdbtk/classify_wf/wrapper.py
@@ -0,0 +1,21 @@
+__author__ = "Alexander Hübner"
+__license__ = "MIT"
+
+from pathlib import Path
+import os
+
+from snakemake.shell import shell
+
+fadir = snakemake.params.get("fadir", "")
+outdir = snakemake.params.get("outdir", "")
+dbdir = snakemake.params.get("dbdir", "")
+
+shell(
+    "(GTDBTK_DATA_PATH={dbdir} "
+    " gtdbtk classify_wf --cpu {snakemake.threads} "
+    "--extension fa --genome_dir {fadir} --out_dir {outdir})"
+)
+
+if not os.path.isfile(snakemake.output[0]):
+    print("WARNING: no MAGs from the kingdom bacterium found!")
+    Path(snakemake.output[0]).touch()
