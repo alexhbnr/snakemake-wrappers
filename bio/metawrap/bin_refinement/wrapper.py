71dae2bd9a68b0ce3b9aeefbc2facd6a5b9e7fb2
Alex Huebner <alexhbnr@gmail.com>
2022-11-14 13:19:42 +0100

    Escape metaWRAP not finding bins

--- bio/metawrap/bin_refinement/wrapper.py
+++ bio/metawrap/bin_refinement/wrapper.py
@@ -3,6 +3,8 @@ __copyright__ = "Copyright 2022, Alexander Hübner"
 __license__ = "MIT"
 
 import os
+from pathlib import Path
+import subprocess
 
 from snakemake.shell import shell
 
@@ -12,12 +14,21 @@ max_contamination = snakemake.params.get("max_contamination", "")
 
 os.makedirs(outdir, exist_ok=True)
 
-shell(
-    "(metawrap bin_refinement -o {outdir} "
-    " -t {snakemake.threads}"
-    " -c {min_completeness}"
-    " -x {max_contamination}"
-    " -A {snakemake.params.maxbin2}"
-    " -B {snakemake.params.metabat2}"
-    " -C {snakemake.params.concoct}) && touch {snakemake.output[0]}"
-)
+try:
+    shell(
+        "(metawrap bin_refinement -o {outdir} "
+        " -t {snakemake.threads}"
+        " -c {min_completeness}"
+        " -x {max_contamination}"
+        " -A {snakemake.params.maxbin2}"
+        " -B {snakemake.params.metabat2}"
+        " -C {snakemake.params.concoct})"
+    )
+except subprocess.CalledProcessError:
+    print("MetaWRAP was not able to find bins. Check the log files!")
+    Path(f"{outdir}/metawrap_50_10_bins.stats").touch()
+
+if not os.path.isdir(f"{outdir}/metawrap_50_10_bins"):
+    os.makedirs(f"{outdir}/metawrap_50_10_bins")
+
+Path(snakemake.output[0]).touch()
